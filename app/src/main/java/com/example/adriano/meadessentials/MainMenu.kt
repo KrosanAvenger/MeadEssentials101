030e4e6dd0e75be912b825a1c966c3404a9d54f9
KrosanAvenger <44968897+KrosanAvenger@users.noreply.github.com>
2018-11-14 23:09:18 +1030

    Initial commit

---
+++ app/src/main/java/com/example/adriano/meadessentials/MainMenu.kt
@@ -0,0 +1,30 @@
+package com.example.adriano.meadessentials
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
